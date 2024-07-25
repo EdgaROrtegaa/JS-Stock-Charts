3e662c0a713e335712ca4556e4f1981915a7b003
EdgaROrtegaa <147570628+EdgaROrtegaa@users.noreply.github.com>
2024-07-25 12:46:02 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -7,3 +7,70 @@ async function main() {
 }
 
 main()
+
+let GME = result.GME
+let MSFT = result.MSFT
+let DIS = result.DIS
+let BTNX = result.BTNX
+
+const { GME, MSFT, DIS, BNTX } = mockData;
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+
+// Bonus Note: 
+// Another way to write the above lines would to refactor it as:
+   // const {GME, MSFT, DIS, BTNX} = result 
+// This is an example of "destructuring" an object
+// "Destructuring" creates new variables from an object or an array
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                '
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                 
+        }]
+    } 
+});
+stocks.forEach( stock => stock.values.reverse())
+
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.map(value => value.datetime),
+        datasets: stocks.map( stock => ({
+            label: stock.meta.symbol,
+            data: stock.values.map(value => parseFloat(value.high)),
+            backgroundColor:  getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+        }))
+    }
+});
+
+
+
+console.log(stocks[0].values)                                                  
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
